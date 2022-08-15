faf113e679b6e3722f2a37e0fb5ca39c5dfa3fed
Andressa Malagutti <48416707+dedevit@users.noreply.github.com>
2022-08-15 11:50:46 -0300

    Update server.py

--- server.py
+++ server.py
@@ -4,32 +4,32 @@ from typing import List, Optional
 from pydantic import BaseModel
 from uuid import uuid4
 
-app = FastAPI()
+app = FastAPI() # Cria a "Instancia FASTAPI"
 
-class Animal(BaseModel):
+class Animal(BaseModel): # Classe que herda de Base model e cria os atributos.
     id:Optional[str]
     nome: str
     sexo: str
     cor: str
 
-banco:List[Animal] = []
+banco:List[Animal] = [] # cria uma lista vazia do tipo lista que contera a lista de animais criada
 
-@app.get ('/')
+@app.get ('/') # endpoint que da pagina inicial
 def root():
     return {'EXERCICIO WOMAKERSCODE - FASTAPI'}
 
-@app.get ('/animais')
+@app.get ('/animais') # endpoint que lista os aninamis criados 
 def listar_animais():
     return banco
 
-@app.get ('/animais/{animal_id}')
+@app.get ('/animais/{animal_id}') # define  o path que será utilizado como filtro de busca dentro do obejto animal
 def obter_animal(animal_id :str):
     for animal in banco:
         if animal.id == animal_id:
             return animal
     return{'error' : "Animal não localizado"}
 
-@app.delete ('/animais/{animal_id}')
+@app.delete ('/animais/{animal_id}') # para deletar o elemento também é utilizado o parâmentro Path
 def remover_animal(animal_id :str):
     posicao = -1
     for index, animal in enumerate(banco):
@@ -43,7 +43,7 @@ def remover_animal(animal_id :str):
         return {'error' : "Animal não localizado"}
 
 
-@app.post ('/animais')
+@app.post ('/animais') # cria o endipoint que irá fazer o cadastro.
 def criar_animal(animal: Animal):
    animal.id = str(uuid4())
    banco.append(animal)
